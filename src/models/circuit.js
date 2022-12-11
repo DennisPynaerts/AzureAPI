e6789485b86ee5b89513226bc6d8a22934cee978
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-12-11 13:41:54 +0100

    change track schema

--- src/models/circuit.js
+++ src/models/circuit.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const tracksSchema = new mongoose.Schema({
-    tracks: [{
+    tracks: {
         id: Number,
         naam: String,
         land: String
-    }]
+    }
 }, {
     collection: 'Tracks',
 });
