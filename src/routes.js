0cfa3444a6fac777a3225968f720a73a008656d0
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-12-17 10:13:34 +0100

    remove next method

--- src/routes.js
+++ src/routes.js
@@ -53,7 +53,6 @@ router.post('/tracks/create', async(req, res) => {
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Max-Age", "1800");
         res.setHeader("Access-Control-Allow-Headers", "content-type");
-        next();
         res.send(await Tracks.create(req.body));
     } catch (e) {
         console.log(e);
